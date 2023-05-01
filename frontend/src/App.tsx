dbcc10c946184272fa80c995db3f126213c53bb0
Vilius Puškunalis <47086537+puskunalis@users.noreply.github.com>
2023-05-01 17:48:37 +0300

    Add router to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,21 @@
 import * as React from "react";
 import { ChakraProvider, SimpleGrid, theme } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
-import ProductAddToCart from "./components/Product";
-import { Product } from "./types";
 import { Provider } from "react-redux";
 import { store } from "./state/store";
 import { ItemsPage } from "./containers/ItemsPage";
-
-const product: Product = {
-  isNew: true,
-  imageURL:
-    "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=4600&q=80",
-  name: "Wayfarer Classic",
-  price: 4.5,
-  rating: 4.2,
-  numReviews: 34,
-};
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 
 export const App = () => (
   <Provider store={store}>
     <ChakraProvider theme={theme}>
       <Navbar />
-      <SimpleGrid columns={8}>
-        <ItemsPage/>
-      </SimpleGrid>
+      <Routes>
+        <Route path="/">
+          <Route index element={<ItemsPage />} />
+          <Route path="kontaktai" element={<h1>Kontaktai</h1>} />
+        </Route>
+      </Routes>
     </ChakraProvider>
   </Provider>
 );
