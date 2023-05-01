9387e46c9d5a6f30f324ad6369d43cb54807e24e
Vilius Puškunalis <47086537+puskunalis@users.noreply.github.com>
2023-05-01 18:25:28 +0300

    Register modal

---
+++ frontend/src/components/Register.tsx
@@ -0,0 +1,55 @@
+import * as React from "react";
+import {
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  FormControl,
+  FormLabel,
+  Input,
+  ModalFooter,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+export function RegisterModal(open: boolean) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+
+  return (
+    <Modal
+      initialFocusRef={initialRef}
+      finalFocusRef={finalRef}
+      isOpen={open}
+      onClose={onClose}
+    >
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Create your account</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <FormControl>
+            <FormLabel>First name</FormLabel>
+            <Input ref={initialRef} placeholder="First name" />
+          </FormControl>
+
+          <FormControl mt={4}>
+            <FormLabel>Last name</FormLabel>
+            <Input placeholder="Last name" />
+          </FormControl>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3}>
+            Save
+          </Button>
+          <Button onClick={onClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
