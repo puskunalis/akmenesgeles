0c6fd6f0834bbb83bdcb599bda4710f4be6b0571
Ignas Balčiūnas <balcignas@gmail.com>
2023-05-27 14:22:10 +0300

    Change logger and log writer configuration

--- backend/src/main/java/com/cementas/akmenesgeles/repository/StrategyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cementas.akmenesgeles.repository;
-
-import com.cementas.akmenesgeles.model.Strategy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StrategyRepository extends JpaRepository<Strategy, Integer> {
-}
